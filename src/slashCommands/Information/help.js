a3fe4884893fc119ae50a24a99beb496301ef64c
Lynx <157897299+lynx707@users.noreply.github.com>
2024-02-19 23:14:01 +0600

    Update help.js

--- src/slashCommands/Information/help.js
+++ src/slashCommands/Information/help.js
@@ -29,35 +29,35 @@ module.exports = {
 
     const embed = new MessageEmbed()
         .setColor(`#DDBD86`)
-          .setAuthor({ name: `${client.user.username}`, iconURL: client.user.displayAvatarURL(), url: `https://discord.gg/aromax-development-708565122188312579` })
+          .setAuthor({ name: `${client.user.username}`, iconURL: client.user.displayAvatarURL(), url: `https://discord.gg/ZtNWe8wSaw` })
 
           .setDescription(`
 <:notes:1119915814733217843> **Music:**
-<:blank:1120331253569302619><:next:1119915811415539722> **/play:** Joins your voice channel and starts playing 24/7.
-<:blank:1120331253569302619><:stop:1119915842893783052> **/stop:** Leaves the voice channel.
-<:blank:1120331253569302619><:dvd:1119915776732827778> **/song:** Shows the current playing song.
-<:blank:1120331253569302619><:radio:1119915830344437790> **/station:** Changes the radio station/theme.
-<:blank:1120331253569302619><:loud:1119915800535511070> **/volume:** Shows or changes the current volume.
-<:blank:1120331253569302619><:sleep:1119915834886856785> **/sleep:** Sets a sleep timer.
+<:blank:1207643268125425695><:next:1119915811415539722> **/play:** Joins your voice channel and starts playing 24/7.
+<:blank:1207643268125425695><:stop:1119915842893783052> **/stop:** Leaves the voice channel.
+<:blank:1207643268125425695><:dvd:1119915776732827778> **/song:** Shows the current playing song.
+<:blank:1207643268125425695><:radio:1119915830344437790> **/station:** Changes the radio station/theme.
+<:blank:1207643268125425695><:loud:1119915800535511070> **/volume:** Shows or changes the current volume.
+<:blank:1207643268125425695><:sleep:1119915834886856785> **/sleep:** Sets a sleep timer.
 <:star:1119915839064379472> **Profiles:**
-<:blank:1120331253569302619><:profile:1119915826326278265> **/profile:** Shows your profile.
-<:blank:1120331253569302619><:am:1121668225839661076> **/remove:** Removes the liked song.
-<:blank:1120331253569302619><:floppy_disk:1119915780931338280> **/collection:** Shows your liked songs collection.
+<:blank:1207643268125425695><:profile:1119915826326278265> **/profile:** Shows your profile.
+<:blank:1207643268125425695><:am:1121668225839661076> **/remove:** Removes the liked song.
+<:blank:1207643268125425695><:floppy_disk:1119915780931338280> **/collection:** Shows your liked songs collection.
 <:config:1119915722534039612> **Config:**
-<:blank:1120331253569302619><:mode:1119915805056966717> **/mode:** Switches between radio modes.
-<:blank:1120331253569302619><:dj:1119915773742288917> **/djrole:** Sets which roles are considered DJs.
-<:blank:1120331253569302619><:gear:1119915784756531331> **/settings:** Shows and configures server settings.
-<:blank:1120331253569302619><:premium:1119915823964893214>  **/premium:** Shows information about Lofi Radio premium.
+<:blank:1207643268125425695><:mode:1119915805056966717> **/mode:** Switches between radio modes.
+<:blank:1207643268125425695><:dj:1119915773742288917> **/djrole:** Sets which roles are considered DJs.
+<:blank:1207643268125425695><:gear:1119915784756531331> **/settings:** Shows and configures server settings.
+<:blank:1207643268125425695><:premium:1119915823964893214>  **/premium:** Shows information about Lofi Radio premium.
 <:info:1119915789030535178> **Info:**
-<:blank:1120331253569302619><:telegram:1119915847809515671> **/support:** Send us a message or [join](https://discord.gg/aromax-development-708565122188312579) our support server.
-<:blank:1120331253569302619><:like:1119915795565269112> **/vote:** Vote for Lofi Radio.
-<:blank:1120331253569302619><:invite:1119915791521955970> **/invite:** [Invite](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot%20applications.commands) Lofi Radio to your server.
+<:blank:1207643268125425695><:telegram:1119915847809515671> **/support:** Send us a message or [join](https://discord.gg/ZtNWe8wSaw) our support server.
+<:blank:1207643268125425695><:like:1119915795565269112> **/vote:** Vote for Lofi Radio.
+<:blank:1207643268125425695><:invite:1119915791521955970> **/invite:** [Invite](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot%20applications.commands) Lofi Radio to your server.
 `)
 
 const b1 = new MessageButton().setLabel(`Play`).setCustomId(`play`).setEmoji(`1119915811415539722`).setStyle('SECONDARY').setDisabled(false)
         const b2 = new MessageButton().setLabel(`Stop`).setCustomId(`stop`).setEmoji(`1119915842893783052`).setStyle('SECONDARY').setDisabled(false)
         const b3 = new MessageButton().setLabel(`Vote`).setEmoji('1119915795565269112')
-    .setURL(`https://discord.gg/aromax-development-708565122188312579`)
+    .setURL(`https://discord.gg/ZtNWe8wSaw`)
 	.setStyle(`LINK`).setDisabled(false)
         const b4 = new MessageButton() .setLabel(`Invite`)
       .setEmoji('1119915791521955970')
@@ -205,7 +205,7 @@ const player = client.manager.players.get(i.guild.id);
         return await i
           .followUp({ embeds: [{
       color: '#DDBD86',
-      description: `👋`
+      description: `**<:error:1207643278711980093> | Erorr**`
     }], ephemeral: true})
           .catch(() => { });
       }
@@ -215,14 +215,14 @@ await player.destroy(interaction.guild.id);
 
 await i.followUp({ embeds: [{
       color: '#DDBD86',
-      description: `<:stop:1119915842893783052> Successfully disconnected from ${i.member.voice.channel}
+      description: `<:stop:1207643335452393492> Successfully disconnected from ${i.member.voice.channel}
  `
     }]})
 } if(player.queue.current == "undefined") {
    return await i
           .followUp({ embeds: [{
       color: '#DDBD86',
-      description: `👋`
+      description: `**<:error:1207643278711980093> | Queue Undefined**`
     }], ephemeral: true})
 }
       
